2004d656546d37f3165a1b0fbd3c9f5756f491c1
Gimwooeung <132888708+Gimwooeung@users.noreply.github.com>
2023-11-28 11:08:42 +0900

    init commit

---
+++ src/main/java/studyin/StudyInApplication.java
@@ -0,0 +1,13 @@
+package studyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyInApplication.class, args);
+    }
+
+}
